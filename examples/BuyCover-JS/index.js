8797e3bb710bc7d68e23a5acdd1394169d315866
Sum <sum.wei@insurace.io>
2021-08-25 15:00:19 +0800

    add BuyCoverExample.sol

--- examples/BuyCover-JS/index.js
+++ examples/BuyCover-JS/index.js
@@ -1,3 +1,23 @@
+/*
+    Copyright (C) 2020 InsurAce.io
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see http://www.gnu.org/licenses/
+*/
+
+// SPDX-License-Identifier: GPL-3.0
+
+
 const axios = require('axios');
 const { Contract, providers, utils, Wallet } = require('ethers');
 
@@ -11,8 +31,8 @@ const jsonRpcUrl = '';
 // The private key of a wallet that is used to purchase this cover.
 const privateKey = '';
 
-// The address of InsurAce Cover contract.
-const contractAddress = '0x0921f628b8463227615D2199D0D3860E4fBcD411';
+// The address of InsurAce Cover contract, ask InsurAce team to get address, different chain has different addresses.
+const contractAddress = 'xxxx';
 
 // The URL of InsurAce API.
 const httpApiUrl = 'https://api.insurace.io/ops/v1';
